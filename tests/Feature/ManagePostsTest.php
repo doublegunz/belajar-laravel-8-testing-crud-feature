2c8743311ae284a1c32f19725cd97b45cd589e77
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-12-31 09:32:35 +0700

    test: create post feature

--- tests/Feature/ManagePostsTest.php
+++ tests/Feature/ManagePostsTest.php
@@ -13,7 +13,37 @@ class ManagePostsTest extends TestCase
     /** @test */
     public function user_can_create_a_post() : void
     {
-        $this->assertTrue(true);
+        // user buka halaman daftar post
+        $this->visit('/post');
+
+        // user klik tombol buat post baru
+        $this->click('create-new-post');
+
+        // user lihat url yang dituju sesuai
+        $this->seePageIs('/post/create');
+
+        // tampil form create post
+        $this->seeElement('form', [
+            'id' => 'create-post',
+            'action' => url('/post/store')
+        ]);
+
+        // user submit form berisi title, content, dan status
+        $this->submitForm('Save', [
+            'title' => 'Belajar Laravel 8',
+            'content' => 'ini content belajar laravel 8',
+            'status' => 1 // publish
+        ]);
+
+        // lihat halaman web ter-redirect ke url sesuai dengan target
+        $this->seePageIs('/post');
+
+        // terdapat record baru sesuai dengan post yang disubmit user
+        $this->seeInDatabase('posts', [
+            'title' => 'Belajar Laravel 8',
+            'content' => 'ini content belajar laravel8',
+            'status' => 1
+        ]);
     }
 
     /** @test */
